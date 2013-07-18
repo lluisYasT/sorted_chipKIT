830ec978014bdfa9af69c18995790a142abe6c1f
Lluis Satorre <lluisyast@gmail.com>
2013-07-18 10:48:02 +0200

    Cambiado el tamaño del array

--- src/numbers.py
+++ src/numbers.py
@@ -1,6 +1,6 @@
 import random
 
 if __name__ == '__main__':
-	for i in range(512):
-		print(random.randint(0,2048), end=" ")
+	for i in range(1024):
+		print(random.randint(0,100000), end=" ")
 	print()
